f1b66500b5f1c76983f4c8d623f3578a15ea83a6
alexis <englebert.alexis@gmail.com>
2024-01-29 15:15:58 +0100

    meurtirer: Add christophe solution & precision in statement

---
+++ meurtrier/submissions/accepted/christophe.py
@@ -0,0 +1,7 @@
+n, sus = int(input()), 0
+names = [input() for _ in range(n)]
+for i in range(1, n):
+    print(f'? {names[sus]} {names[i]}')
+    ans = input()
+    if ans == 'NON': sus = i
+print(f'! {names[sus]}')
