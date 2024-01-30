9bec0a207fb72876ed6c3c73d556b62d38aead94
alexisloic21 <englebert.alexis@gmail.com>
2024-01-30 15:02:40 +0100

    24hvelo: fix output validator

--- 24hvelo/output_validators/24hvelo_validator/output_validator.cpp
+++ 24hvelo/output_validators/24hvelo_validator/output_validator.cpp
@@ -34,34 +34,33 @@ int main(int argc, char *argv[]) {
     int sum = 0;
     int target_sum = 0;
 
-    if(student_n == ans_n){
-        if(ans_n == -1){
-            return 42;
+    
+    if(ans_n == -1 && student_n == -1){
+        return 42;
+    } else if(ans_n != -1 && student_n == -1 || ans_n == -1 && student_n != -1){
+        v.WA("Expected -1 but got something else");
+    }
+
+    // read ans target sum
+    for (int i = 0; i < ans_n; i++) {
+        string s; ans >> s;
+        target_sum += mapper[s];
+    }
+
+    // check if student target doesn't include more than 1 item
+    for(int i = 0; i < student_n; i++ ) {
+        string s = v.read_string("item", 1, (int)10e6);
+        v.newline();
+        cnt[s]++;
+        if (cnt[s] > 1) {
+            v.WA("You can't take the same item twice");
         }
 
-        // read ans target sum
-        for (int i = 0; i < ans_n; i++) {
-            string s; ans >> s;
-            target_sum += mapper[s];
-        }
+        sum += mapper[s];
+    }
 
-        // check if student target doesn't include more than 1 item
-        for(int i = 0; i < student_n; i++ ) {
-            string s = v.read_string("item", 1, (int)10e6);
-            v.newline();
-            cnt[s]++;
-            if (cnt[s] > 1) {
-                return 43;
-            }
-            sum += mapper[s];
-        }
-
-        if(sum != target_sum){
-            return 43;
-        }
-
-    }else{
-        return 43;
+    if(sum != target_sum){
+        v.WA("The sum of the items you took is not equal to the target sum");
     }
     
 }
